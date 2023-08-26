db090cd085592bc6267256eb6b36246f625f6de8
Ed Carlos Fernandes <92828340+EdCarlos-Fernandes@users.noreply.github.com>
2023-08-25 23:42:16 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,108 @@
+<?php 
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    require_once './_config/config.php';
+    require_once './_config/data.php';
+
+    if(isset($_SESSION["numLogin"])){
+        if(isset($_GET["num"])){
+            $n1=$_GET["num"];
+            
+        }else if(isset($_POST["num"])){
+            $n1=$_POST["num"];
+        }
+        
+        $n2=$_SESSION["numLogin"];
+        
+        if($n1!=$n2){
+            header("Location: $url/index.php");
+            exit();
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $titulo ?> - INICIO</title>
+    <?php 
+        require_once __DIR__ . '/_layout/head.php';
+    ?>
+</head>
+
+<body>
+    <header style="background-color: #000;">
+        <?php 
+            require_once __DIR__ . '/_layout/cabecalho.php';
+        ?>
+    </header>
+
+
+    <main class="container-fluid page-body-wrapper" style="padding-top: 50px;">
+        <div class="container px-4 px-lg-5">
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0"src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg" alt="..." /></div>
+                <div class="col-lg-5">
+                    <h1 class="font-weight-light"><?php echo $nomeUser; ?> Name or Tagline</h1>
+                    <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to
+                        it, but it makes a great use of the standard Bootstrap core components. Feel free to use this
+                        template for any project you want!</p>
+                    <a class="btn btn-primary" href="#!">Call to Action!</a>
+                </div>
+            </div>
+            <!-- Call to Action-->
+            <div class="card text-white bg-secondary my-5 py-4 text-center">
+                <div class="card-body">
+                    <p class="text-white m-0">This call to action card is a great place to showcase some important
+                        information or display a clever tagline!</p>
+                </div>
+            </div>
+            <!-- Content Row-->
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Card One</h2>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni
+                                quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Card Two</h2>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur
+                                ex natus at dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id
+                                dolore voluptates quos eligendi labore.</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Card Three</h2>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni
+                                quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+    <?php 
+        require_once __DIR__ . "/_layout/rodape.php";
+        require_once __DIR__ . '/_layout/scriptFooter.php';
+    ?>
+</body>
+
+</html>
